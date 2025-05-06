2cab36b0163959bb247e27754b7ba074cb632049
GaliEvdokimova <galina.evdokimova5@icloud.com>
2025-05-06 22:16:26 +0400

    check2

--- Tracker/TabBar/TabBarController.swift
+++ Tracker/TabBar/TabBarController.swift
@@ -15,7 +15,7 @@ final class TabBarController: UITabBarController {
         trackersViewController.tabBarItem = UITabBarItem(title: "Трекеры", image: UIImage(named: "Trackers"), selectedImage: nil)
         
         let statisticsViewController = StatisticsViewController()
-        statisticsViewController.tabBarItem = UITabBarItem(title: "Статистика", image: UIImage(named: "stats"), selectedImage: nil)
+        statisticsViewController.tabBarItem = UITabBarItem(title: "Статистика", image: UIImage(named: "bunny"), selectedImage: nil)
         self.viewControllers = [trackersViewController, statisticsViewController]
         
         if #available(iOS 13.0, *) {
